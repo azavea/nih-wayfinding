a5cc741a415a5de9faa1e11171a2af44ad467e0e
CloudNiner <andrew.fink.11@gmail.com>
2015-01-05 12:14:42 -0500

    Add boilerplate angular app
    Remove unnecessary htaccess file

---
+++ src/nih_wayfinding/app/scripts/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name nihWayfindingApp
+ * @description
+ * # nihWayfindingApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('nihWayfindingApp', [
+    'ngAnimate',
+    'ngAria',
+    'ngCookies',
+    'ngTouch'
+  ]);
