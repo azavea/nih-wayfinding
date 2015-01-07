aa00fdf54e91906f85aa80a77b1a0b28051b848a
CloudNiner <andrew.fink.11@gmail.com>
2015-01-07 14:27:17 -0500

    Replace OSM basemap with Stamen Toner Lite

--- src/nih_wayfinding/app/scripts/config.js
+++ src/nih_wayfinding/app/scripts/config.js
@@ -1,6 +1,8 @@
-
 (function () {
     'use strict';
+
+    var osmAttribution = 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, ' +
+    '<a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>';
 
     var config = {
         bounds: {
@@ -13,10 +15,26 @@
                 lng: -87.523984
             }
         },
+        baseLayers: {
+            stamentonerlite: {
+                name: 'Stamen Toner Lite',
+                type: 'xyz',
+                url: 'https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}.png',
+                layerOptions: {
+                    attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, ' +
+                        '<a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> ' +
+                        '&mdash; ' + osmAttribution,
+                    subdomains: ['a', 'b', 'c', 'd'],
+                    minZoom: 3,
+                    maxZoom: 20,
+                    continuousWorld: true
+                }
+            }
+        },
         center: {
             lat: 41.8749,
             lng: -87.6189,
-            zoom: 14
+            zoom: 15
         }
     };
 
