1c6e1527cca0ee41452900195ce0e271330ef8fc
Nathan Zimmerman <npzimmerman@gmail.com>
2015-01-14 10:15:07 -0500

    Use notifications to alert people when profile is not complete

--- src/nih_wayfinding/app/scripts/views/locations/add-location/location-profile/location-profile-controller.js
+++ src/nih_wayfinding/app/scripts/views/locations/add-location/location-profile/location-profile-controller.js
@@ -15,10 +15,10 @@
             ctl.currentLocation = UserLocations.getLocationByID($stateParams.id) ||
                 UserLocations.getWorkingLocation();
             ctl.saveCurrent = saveCurrent;
+
+            // Hidden image selection dialog
             ctl.showIconSelect = false;
             ctl.toggleIconSelectDialog = toggleIconSelectDialog;
-            ctl.showIconUpload = false;
-            ctl.toggleIconUploadDialog = toggleIconUploadDialog;
             ctl.imgOpts = [
                 { img: 'http://lorempixel.com/200/100/' },
                 { img: 'http://lorempixel.com/200/100/' },
@@ -27,16 +27,24 @@
                 { img: 'http://lorempixel.com/200/100/' },
                 { img: 'http://lorempixel.com/200/100/' },
             ];
+            ctl.imgOptionClicked = imgOptionClicked;
+
+            // Hidden upload dialog
+            ctl.showIconUpload = false;
+            ctl.toggleIconUploadDialog = toggleIconUploadDialog;
             ctl.uploadOpts = [
                 { text: 'Select File' },
                 { text: 'Use Camera' }
             ];
-            ctl.imgOptionClicked = imgOptionClicked;
+            ctl.uploadOptionClicked = uploadOptionClicked;
 
             // Search vars
             ctl.findAddressExpanded = false;
             ctl.search = search;
             ctl.suggest = Geocoder.suggest;
+
+            // Final validation
+            ctl.validateBeforeReview = validateBeforeReview;
         }
 
         function search(searchText) {
@@ -47,12 +55,8 @@
             if (data.length) { // If non-empty result
                 // Add geometric features to location tracker
                 ctl.currentLocation.feature = data[0];
-                saveCurrent(ctl.currentLocation);
-
-                var geom = data[0].geometry; // Grab first result
-                var xyString = geom.x.toString() + ',' + geom.y.toString(); // Cast to string
-                $state.go('locationsReview', { destination: xyString }); // Use as url params
             } else { // If empty result
+                ctl.currentLocation.feature = undefined;
                 Notifications.show({
                     text: 'Unable to find the selected address. Please try a different one.',
                     timeout: 3000
@@ -73,12 +77,35 @@
             UserLocations.setLocationFeature(location.feature);
         }
 
+        function uploadOptionClicked(option) {
+            console.log(option.text);
+        }
+
         function toggleIconSelectDialog() {
             ctl.showIconSelect = !ctl.showIconSelect;
         }
 
         function toggleIconUploadDialog() {
             ctl.showIconUpload = !ctl.showIconUpload;
+        }
+
+        function validateBeforeReview() {
+            if (ctl.currentLocation.text === undefined) { // If there's no label
+                Notifications.show({
+                    text: 'No label specified - please label this location.',
+                    timeout: 3000
+                });
+            } else if (ctl.currentLocation.feature === undefined) { // If address fails to validate
+                Notifications.show({
+                    text: 'No coordinates found for this address - please choose an address.',
+                    timeout: 3000
+                });
+            } else { // If we have both a label and an address
+                saveCurrent(ctl.currentLocation);
+                var geom = ctl.currentLocation.feature.geometry;
+                var xyString = geom.x.toString() + ',' + geom.y.toString(); // Cast to string
+                $state.go('locationsReview', { destination: xyString }); // Use as url params
+            }
         }
 
     }
