4428c0e368c0ce0d6263a0c19a78a3163ea20e81
CloudNiner <andrew.fink.11@gmail.com>
2015-01-29 16:16:56 -0500

    Add dynamic stepping along navigated route

--- src/nih_wayfinding/app/scripts/navigation/navigation-service.js
+++ src/nih_wayfinding/app/scripts/navigation/navigation-service.js
@@ -16,8 +16,11 @@
             latitude: 0,
             longitude: 0
         };
-        var currentIndex = 0;
-        var currentRoute = [];  // An array of [lon, lat]
+        var stepLengthFeet = Config.stepLengthFeet || 100;
+        var stepLengthMiles = stepLengthFeet / 5280;
+        var lineDistance = 0;
+        var steppedDistance = 0;
+        var currentRoute = null;  // A linestring
         var module = {
             getCurrentPosition: getCurrentPosition,
             setRoute: setRoute,
@@ -56,15 +59,19 @@
         }
 
         function setRoute(newRoute) {
-            currentRoute = newRoute;
+            currentRoute = turf.linestring(newRoute);
+            lineDistance = turf.lineDistance(currentRoute, 'miles');
+            steppedDistance = 0;
         }
 
         function stepNext() {
-            setPosition(currentIndex + 1);
+            steppedDistance += stepLengthMiles;
+            setPosition(steppedDistance);
         }
 
         function stepPrevious() {
-            setPosition(currentIndex - 1);
+            steppedDistance -= stepLengthMiles;
+            setPosition(steppedDistance);
         }
 
         function stepFirst() {
@@ -72,21 +79,61 @@
         }
 
         function stepLast() {
-            setPosition(currentRoute.length - 1);
+            setPosition(lineDistance);
         }
 
-        function setPosition(index) {
-            index = parseInt(index, 10);
-            if (routeExists() && index >= 0 && index < currentRoute.length) {
-                position.latitude = currentRoute[index][1];
-                position.longitude = currentRoute[index][0];
-                currentIndex = index;
+        function setPosition(distance) {
+            if (distance > lineDistance) {
+                distance = lineDistance;
+                steppedDistance = lineDistance;
+            }
+            if (distance < 0) {
+                distance = 0;
+                steppedDistance = 0;
+            }
+            if (routeExists()) {
+                var point = along(currentRoute, distance, 'miles');
+                position.latitude = point.geometry.coordinates[1];
+                position.longitude = point.geometry.coordinates[0];
                 $rootScope.$broadcast(events.positionUpdated, position);
             }
         }
 
         function routeExists() {
-            return _.isArray(currentRoute) && currentRoute.length;
+            return !!(currentRoute);
+        }
+
+        /**
+         * Temporary reimplementation of turf.along() that includes the fix mentioned here:
+         * https://github.com/Turfjs/turf-along/pull/1
+         *
+         * TODO: Replace with turf.along call once the fix above is merged and turf is updated
+         *
+         * Params and return match turf.along()
+         */
+        function along(line, dist, units) {
+            var coords;
+            if(line.type === 'Feature') coords = line.geometry.coordinates;
+            else if(line.type === 'LineString') coords = line.geometry.coordinates;
+            else throw new Error('input must be a LineString Feature or Geometry');
+
+            var travelled = 0;
+            for(var i = 0; i < coords.length; i++) {
+                if (dist >= travelled && i === coords.length - 1) break;
+                else if(travelled >= dist) {
+                    var overshot = dist - travelled;
+                    if(!overshot) return turf.point(coords[i]);
+                    else {
+                        var direction = turf.bearing(turf.point(coords[i]), turf.point(coords[i-1])) - 180;
+                        var interpolated = turf.destination(turf.point(coords[i]), overshot, direction, units);
+                        return interpolated;
+                    }
+                }
+                else {
+                    travelled += turf.distance(turf.point(coords[i]), turf.point(coords[i+1]), units);
+                }
+            }
+            return turf.point(coords[coords.length - 1]);
         }
     }
 
