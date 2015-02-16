270ce1beb6917878c9062a50d57098f4bb9345d0
CloudNiner <andrew.fink.11@gmail.com>
2015-02-16 10:06:59 -0500

    Remove stubbed turf.along

--- src/nih_wayfinding/app/scripts/navigation/navigation-service.js
+++ src/nih_wayfinding/app/scripts/navigation/navigation-service.js
@@ -153,7 +153,7 @@
                 currentRoute.stepped = 0;
             }
             if (routeExists()) {
-                var point = along(currentRoute.geom, distance, 'miles');
+                var point = turf.along(currentRoute.geom, distance, 'miles');
                 var stepBox = findStepBoxForPoint(point);
                 // Pass all relevant position info
                 //  point, properties, next destination point
@@ -225,41 +225,6 @@
             return _.find(currentRoute.stepBoxes, function (stepBox) {
                 return turf.inside(point, stepBox.polygon);
             });
-        }
-
-        /**
-         * Temporary reimplementation of turf.along() that includes the fix mentioned here:
-         * https://github.com/Turfjs/turf-along/pull/1
-         *
-         * TODO: Replace with turf.along call once the fix above is merged and turf is updated
-         *
-         * Params and return match turf.along()
-         */
-        function along(line, dist, units) {
-            /* jshint curly:false */
-            var coords;
-            if(line.type === 'Feature') coords = line.geometry.coordinates;
-            else if(line.type === 'LineString') coords = line.geometry.coordinates;
-            else throw new Error('input must be a LineString Feature or Geometry');
-
-            var travelled = 0;
-            for(var i = 0; i < coords.length; i++) {
-                if (dist >= travelled && i === coords.length - 1) break;
-                else if(travelled >= dist) {
-                    var overshot = dist - travelled;
-                    if(!overshot) return turf.point(coords[i]);
-                    else {
-                        var direction = turf.bearing(turf.point(coords[i]), turf.point(coords[i-1])) - 180;
-                        var interpolated = turf.destination(turf.point(coords[i]), overshot, direction, units);
-                        return interpolated;
-                    }
-                }
-                else {
-                    travelled += turf.distance(turf.point(coords[i]), turf.point(coords[i+1]), units);
-                }
-            }
-            return turf.point(coords[coords.length - 1]);
-            /* jshint curly:true */
         }
     }
 
